15507276ae71459dcccddb7b001008adf0833ca4
csmueed <csmueed@gmail.com>
2026-06-29 11:36:54 +0500

    add features

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ const toDoInput = document.querySelector("#todo-input");
 const addTask = document.querySelector("#add-task-btn");
 const toDoList = document.querySelector("#todo-list");
 
-const all = document.querySelector(".all");
-const pending = document.querySelector(".pending");
-const completed = document.querySelector(".completed");
+const filterBtn = document.querySelectorAll(".filter-btn");
 
 const taskArray = [];
 
@@ -20,6 +18,8 @@ class Tasks {
   }
 }
 
+loadFromLocal();
+
 addTask.addEventListener("click", (e) => {
   e.preventDefault();
   inputValue = toDoInput.value.trim();
@@ -28,7 +28,13 @@ addTask.addEventListener("click", (e) => {
     let taskOne = new Tasks(inputValue);
     taskArray.push(taskOne);
     console.log(taskArray);
-    toDoList.innerHTML += `<li class = "listItem" data-id = ${taskOne.id}> <div class="task-test"> ${inputValue} </div> <div> <button class="btn-edit"> Edit </button> </div> <div> <button class="btn-delete"> Delete </button> </div> <div> <input type="checkbox" class="check" </div> </li>`;
+    // toDoList.innerHTML += `<li class = "listItem" data-id = ${taskOne.id}> <div class="task-test"> ${inputValue} </div> <div> <button class="btn-edit"> Edit </button> </div> <div> <button class="btn-delete"> Delete </button> </div> <div> <input type="checkbox" class="check" </div> </li>`;
+    let myLi = document.createElement("li");
+    myLi.className = "listItem";
+    myLi.dataset.id = `${taskOne.id}`;
+    myLi.innerHTML = `<div class="task-test"> ${inputValue} </div> <div> <button class="btn-edit"> Edit </button> </div> <div> <button class="btn-delete"> Delete </button> </div> <div> <input type="checkbox" class="check" </div>`;
+    toDoList.appendChild(myLi);
+    saveToLocal();
     // allLiItems = document.querySelectorAll("li");
     toDoInput.value = "";
   } else if (editMode === true) {
@@ -47,7 +53,14 @@ addTask.addEventListener("click", (e) => {
 toDoList.addEventListener("click", (e) => {
   if (e.target.classList.contains("btn-delete")) {
     let myLi = e.target.parentElement.parentElement;
+    let dataID = myLi.dataset.id;
     myLi.remove();
+    let taskArrIndexId = taskArray.findIndex((index) => {
+      return index.id === dataID;
+    });
+    taskArray.splice(taskArrIndexId, 1);
+    console.log(taskArray);
+    saveToLocal();
   } else if (e.target.classList.contains("check")) {
     let myLi = e.target.parentElement.parentElement;
     const htmlDataID = myLi.dataset.id;
@@ -55,6 +68,7 @@ toDoList.addEventListener("click", (e) => {
       return item.id === htmlDataID;
     });
     value.done = !value.done;
+    saveToLocal();
   } else if (e.target.classList.contains("btn-edit")) {
     myLiEdit = e.target.parentElement.parentElement;
     htmlDataID = myLiEdit.dataset.id;
@@ -64,7 +78,18 @@ toDoList.addEventListener("click", (e) => {
     toDoInput.value = value.task;
     editedText = value.task;
     editMode = true;
+    saveToLocal();
   }
+});
+
+filterBtn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    filterBtn.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    filteredItems(e.target.dataset.filter);
+  });
 });
 
 function filteredItems(parameter) {
@@ -76,7 +101,7 @@ function filteredItems(parameter) {
         return value.id === htmlID;
       });
       if (filteredObj.done === true) {
-        item.style.display = "grid";
+        item.style.display = "flex";
       }
       if (filteredObj.done === false) {
         item.style.display = "none";
@@ -104,6 +129,29 @@ function filteredItems(parameter) {
       });
       if (filteredObj.done === true || filteredObj.done === false) {
         item.style.display = "flex";
+      }
+    });
+  }
+}
+
+function saveToLocal() {
+  localStorage.setItem("tasks", JSON.stringify(taskArray));
+}
+
+function loadFromLocal() {
+  let tasksLocal = localStorage.getItem("tasks");
+  if (tasksLocal) {
+    let parsedItem = JSON.parse(tasksLocal);
+    taskArray.push(...parsedItem);
+    parsedItem.forEach((item) => {
+      let myLi = document.createElement("li");
+      myLi.className = "listItem";
+      myLi.dataset.id = `${item.id}`;
+      myLi.innerHTML = `<div class="task-test"> ${item.task} </div> <div> <button class="btn-edit"> Edit </button> </div> <div> <button class="btn-delete"> Delete </button> </div> <div> <input type="checkbox" class="check" </div>`;
+      toDoList.appendChild(myLi);
+      if (item.done === true) {
+        myLi.classList.add("completed");
+        myLi.querySelector(".check").checked = true;
       }
     });
   }
